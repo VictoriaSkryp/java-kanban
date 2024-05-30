880a254990893b3cb6da757de896dae4cd7e6a5b
VictoriaSkryp <victoria.skrypkovich@yandex.kz>
2024-05-30 21:46:19 +0300

    fix(task-manager): fix review

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -7,19 +7,16 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void add(Task task) {
-        tasksHistory.add(task);
+        if (tasksHistory.size() == 10) {
+            tasksHistory.removeLast();
+            tasksHistory.add(task);
+        } else {
+            tasksHistory.add(task);
+        }
     }
 
     @Override
     public List<Task> getHistory() {
-        ArrayList<Task> result = new ArrayList<>();
-        if (!tasksHistory.isEmpty() && tasksHistory.size() < 10) {
-            result.addAll(tasksHistory);
-        } else if (!tasksHistory.isEmpty()) {
-            for (int i = 0; i < 10; i++) {
-                result.add(tasksHistory.get(i));
-            }
-        }
-        return result;
+        return tasksHistory;
     }
 }
